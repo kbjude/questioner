6c9e37d11417125c76087b888ed8022e53782a74
Zack Atama <atama.zack@hotmail.com>
2019-03-06 04:07:02 +0300

    chore(travis integration): add travis ci integration
    this is for test coverage purposes
    
    [Chores #164393845]

--- questioner/travis_ci_settings.py
+++ questioner/travis_ci_settings.py
@@ -11,3 +11,12 @@ DATABASES = {
         'HOST': '127.0.0.1',
     }
 }
+MIDDLEWARE_CLASSES = [
+    'django.middleware.security.SecurityMiddleware',
+    'django.contrib.sessions.middleware.SessionMiddleware',
+    'django.middleware.common.CommonMiddleware',
+    'django.middleware.csrf.CsrfViewMiddleware',
+    'django.contrib.auth.middleware.AuthenticationMiddleware',
+    'django.contrib.messages.middleware.MessageMiddleware',
+    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+]
