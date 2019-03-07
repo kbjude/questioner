807c6549882ffaa6a5691c961a4dcbd6ec66f99f
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-03-07 14:02:00 +0300

    Update urls.py

--- meetup/urls.py
+++ meetup/urls.py
@@ -1,7 +1,12 @@
-from django.urls import path
-from .views import Index
+from django.conf.urls import url
+from . import views
 
 
 urlpatterns = [
-    path('', Index.as_view()),
+    # /meetups/
+    url(r'^$', views.MeetingList.as_view(), name='meetings'),
+
+    # /meetups/234/
+    url(r'^(?P<meeting_id>[0-9]+)$',
+        views.AMeeting.as_view(), name='meeting'),
 ]
