26db4c60d14d14665ff54527d86caf1b3564e227
Kalule Arthur <kalulearthur@gmail.com>
2019-03-07 09:09:09 +0300

    Remove unused import

--- questioner/urls.py
+++ questioner/urls.py
@@ -14,7 +14,7 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.conf.urls import include, url
+from django.conf.urls import url
 from rest_framework.urlpatterns import format_suffix_patterns
 from . import views
 
