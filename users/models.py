be534f128579020ad49763b7f896b9239edc5b5f
Jean Mark <kabohajeanmark@gmail.com>
2019-03-08 20:08:22 +0300

    Work on user signup with all credentials [finishes #164364407]

--- users/models.py
+++ users/models.py
@@ -4,7 +4,12 @@ from django.db import models
 class QuestionerUsers(models.Model):
     firstname = models.CharField(max_length=20)
     lastname = models.CharField(max_length=20)
-    is_admin = models.BooleanField()
+    username = models.CharField(max_length=20)
+    password = models.CharField(max_length=20)
+    email = models.CharField(max_length=20)
+    is_admin = models.BooleanField(default=False)
+    phone_number = models.CharField(max_length=20)
+    date_created = models.DateTimeField(editable=False)
 
     def __str__(self):
         # return self.title, self.date, self.start, self.end, self.created_by
--- users/views.py
+++ users/views.py
@@ -1,3 +1,30 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+from django.views.decorators.csrf import csrf_exempt
+from django.utils import timezone
+import json
+from users.models import QuestionerUsers
 
-# Create your views here.
+@csrf_exempt
+def user_signup(request):
+    '''
+        this views method is responsible for the following;
+        - creating new users, user signup
+        - getting all users from the database
+    '''
+    if request.method == 'POST':
+        payload = json.loads(request.body)
+        firstname = payload['firstname']
+        lastname = payload['lastname']
+        username = payload['username']
+        password = payload['password']
+        email = payload['email']
+        phonenumber = payload['phonenumber']
+
+        user = QuestionerUsers(firstname=firstname, lastname=lastname, username=username, password=password, email=email, phone_number=phonenumber, date_created = timezone.now())
+        try:
+            user.save()
+            response = json.dumps([{'message': 'user successfully registered'}]),201          
+            
+        except:
+            response = json.dumps([{'error': 'user could not be registered'}]),400
