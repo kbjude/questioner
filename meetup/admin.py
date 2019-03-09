da3da7a36ec8bd7eb2be3bad52839ad1efb2711a
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-03-09 14:12:45 +0300

    Update admin.py

--- meetup/admin.py
+++ meetup/admin.py
@@ -1,4 +1,8 @@
 from django.contrib import admin
 from .models import Meeting
+from .models import Tag
+from .models import MeetingTag
 
 admin.site.register(Meeting)
+admin.site.register(Tag)
+admin.site.register(MeetingTag)
