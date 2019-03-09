56dfcabba0e72294a1697feb0bad0441f1331688
mwinel <mwinel@live.com>
2019-03-09 10:05:59 +0300

    ft(add-comment): Add docstrings to models

--- question/models.py
+++ question/models.py
@@ -1,11 +1,14 @@
 from django.db import models
 
 class Question(models.Model):
+    """This class represents the question model."""
+
     title = models.CharField(max_length=100)
     body = models.TextField()
     created_by = models.IntegerField()
     date_created = models.DateTimeField(editable=False)
     date_modified = models.DateTimeField()
-    def __str__(self):
-        return (self.title, self.body)
 
+    def __str__(self):
+        """Return a readable representation of the model instance."""
+        return (self.title, self.body)
