b1b5d96a8c8b8b4cbe2830d5609b3f3e434c3dbe
Jean Mark <kabohajeanmark@gmail.com>
2019-03-09 20:19:19 +0300

    Register app users[finishes #164364407]

--- questioner/settings.py
+++ questioner/settings.py
@@ -39,7 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'meetup.apps.MeetupConfig',
-
+    'users.apps.UsersConfig',
     'rest_framework',
     'rest_framework.authtoken',
 ]
