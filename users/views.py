4a77ce9d29246efa85c8240cd5cc6c68e25acb54
Jean Mark <kabohajeanmark@gmail.com>
2019-03-09 20:17:31 +0300

    Add view for signup[#finishes #164364407]

--- users/views.py
+++ users/views.py
@@ -1,3 +1,34 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from rest_framework import status
+from rest_framework.permissions import IsAuthenticated
+from .serializers import UserSerializer
+from django.contrib.auth.models import User
+from rest_framework.authtoken.views import ObtainAuthToken
+from rest_framework.authtoken.models import Token
+from rest_framework import status
 
-# Create your views here.
+class SignUp(APIView):
+    """
+    Register a user.
+    """
+
+    def post(self, request, format='json'):
+        serializer = UserSerializer(data=request.data)
+        if serializer.is_valid():
+            user = serializer.save()
+            if user:
+                return Response({
+                    'status': status.HTTP_201_CREATED,
+                    'data': [{'user_id': user.pk,
+                              'username': user.username,
+                              'email': user.email,
+                              'is_admin': user.is_superuser
+                              }]
+                })
+        else:
+            return Response({
+                'status': 400,
+                'errors': serializer.errors
+            })
