64c9b06930b933771342b6d9bbe693e11ad09fe7
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-03-09 21:19:18 +0300

    Update urls.py

--- meetup/urls.py
+++ meetup/urls.py
@@ -3,7 +3,6 @@ from . import views
 
 
 urlpatterns = [
-    path('', views.Index.as_view(), name='index'),
     path('auth/login/', views.Login.as_view(), name='login'),
     path('auth/signup/', views.SignUp.as_view(), name='signup'),
 
