a27d9ef131d484701d056de136f598cebeb6c516
mwinel <mwinel@live.com>
2019-03-12 23:32:06 +0300

    Add auth user serializer

--- question/serializers.py
+++ question/serializers.py
@@ -1,4 +1,5 @@
 from rest_framework import serializers
+from django.contrib.auth.models import User
 from question.models import Question, Comment
 
 
@@ -17,12 +18,25 @@ class CommentSerializer(serializers.ModelSerializer):
 class QuestionSerializer(serializers.ModelSerializer):
     
     comments = CommentSerializer(many=True, read_only=True)
+
     class Meta:
         model = Question
         fields = ('id', 'title', 'body', 'created_by', 'date_created', 
                   'date_modified', 'comments')
         read_only_fields = ('date_created', 'date_modified')
-        
+
+
+class UserSerializer(serializers.ModelSerializer):
+    """Serializer to aid in Authentication and Authorization."""
+
+    questions = serializers.PrimaryKeyRelatedField(
+        many=True, queryset=Question.objects.all())
+
+    class Meta:
+        """Map serializer fields to the user."""
+        model = User
+        fields = ('id', 'username', 'questions')
+
 
 # class VoteSerializer(serializers.ModelSerializer):
 #     class Meta:
