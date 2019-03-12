70c669cd8f86d4ab76627fce091179d272609bf3
mwinel <mwinel@live.com>
2019-03-12 23:36:44 +0300

    Add owner permission

---
+++ question/permissions.py
@@ -0,0 +1,13 @@
+from rest_framework import permissions
+
+
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission class to allow only question owners update them."""
+
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions are only allowed to the owner
+        return obj.created_by == request.user
