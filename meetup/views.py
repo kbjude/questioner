b8ec202ed54c98e6f1a1a90345625d99196f53ca
Kalule Arthur <kalulearthur@gmail.com>
2019-03-12 12:07:39 +0300

    Reformat code

--- meetup/views.py
+++ meetup/views.py
@@ -2,11 +2,11 @@ import datetime
 
 from django.shortcuts import get_object_or_404
 from rest_framework import status
-from rest_framework.permissions import IsAuthenticated, IsAdminUser
+from rest_framework.permissions import IsAdminUser
+from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
 from rest_framework.views import APIView
-from rest_framework.permissions import IsAuthenticated
-from .permissions import IsOwner
+
 from .models import Meeting
 from .models import MeetingTag
 from .models import Tag
