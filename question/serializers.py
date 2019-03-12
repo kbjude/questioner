f2855d3acc562ed5ea11610f52b7e9baa59777d2
mwinel <mwinel@live.com>
2019-03-12 23:19:36 +0300

    Add comment serializer

--- question/serializers.py
+++ question/serializers.py
@@ -1,7 +1,19 @@
 from rest_framework import serializers
+from question.models import Question, Comment
 
-from question.models import Question
 
+class CommentSerializer(serializers.ModelSerializer):
+    """Map the comment model instance into JSON format."""
+
+    created_by = serializers.ReadOnlyField(source='created_by.username')
+
+    class Meta:
+        """Map serializer fields to comment model fields."""
+
+        model = Comment
+        fields = ('id', 'comment', 'created_by', 'date_created', 'date_modified')
+        read_only_fields = ('date_created', 'date_modified')
+        
 
 class QuestionSerializer(serializers.ModelSerializer):
     class Meta:
