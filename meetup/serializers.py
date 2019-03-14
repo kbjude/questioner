8114ad3083d30749c0b05d05af0b0601175d1b67
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-03-14 09:23:49 +0300

    Update serializers.py

--- meetup/serializers.py
+++ meetup/serializers.py
@@ -9,7 +9,7 @@ class MeetingSerializer(serializers.ModelSerializer):
     class Meta:
         model = Meeting
         fields = '__all__'
-        # fields = ('title', 'date', 'start', 'end', 'created_by')
+        # fields = ('id', 'title', 'date', 'start', 'end')
 
 
 class MeetingTagSerializer(serializers.ModelSerializer):
