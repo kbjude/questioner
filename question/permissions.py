bc86fae896277231782836166e338a92dca60921
mwinel <mwinel@live.com>
2019-03-14 23:19:45 +0300

    Add comment CRUD

--- question/permissions.py
+++ question/permissions.py
@@ -3,7 +3,8 @@ from rest_framework import permissions
 
 class IsOwnerOrReadOnly(permissions.BasePermission):
     """
-    Custom permission class to allow only question owners update them."""
+    Custom permission class to allow only question owners update them.
+    """
 
     def has_object_permission(self, request, view, obj):
         # Read permissions are allowed to any request
