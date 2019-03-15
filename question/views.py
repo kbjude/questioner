90cd6de40eef428df8d5564f93f0dad10e4223ca
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-03-15 21:09:07 +0300

    Update views.py

--- question/views.py
+++ question/views.py
@@ -65,10 +65,10 @@ class Questions(APIView):
                 votes = [{"up votes": up_votes, "down votes": dwn_votes}]
 
                 Mserializer = MeetingSerializer(mymeeting, many=True)
-                
+
                 user = User.objects.filter(Q(id=result["created_by"])).distinct().first()
                 result["created_by_name"] = user.username
-                           
+
                 result["meetup_name"] = Mserializer.data[0]["title"]
                 result["votes"] = votes
 
