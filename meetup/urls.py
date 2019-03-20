e7460994a3cf417f1373bc40034f8cd59b220b50
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-03-20 17:51:20 +0300

    Update urls.py

--- meetup/urls.py
+++ meetup/urls.py
@@ -1,23 +1,23 @@
 from django.urls import path
 
 from . import views
+from tag import views as tag_views
 
 urlpatterns = [
     # /meetups/
     path("", views.MeetingList.as_view(), name="meetings"),
     # /meetups/234/
     path("<int:meeting_id>", views.AMeeting.as_view(), name="meeting"),
-    # /meetups/{meet_up_id}tags/
+    # /meetups/{meet_up_id}/tags/
     path(
         "<int:meeting_id>/tags/",
-        views.AddMeetupTag.as_view(),
+        tag_views.AddMeetupTag.as_view(),
         name="meetingtags",
     ),
     # /meetups/234/tags/432
     path(
         "<int:meeting_id>/tags/<int:tag_id>",
-        views.AmeetupTag.as_view(),
+        tag_views.AmeetupTag.as_view(),
         name="meetingtag",
     ),
-
 ]
