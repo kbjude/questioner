8b363b8c0dd8310598f77e3a7c5373b90dd7881b
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-03-20 14:24:19 +0300

    Update permissions.py

--- question/permissions.py
+++ question/permissions.py
@@ -1,14 +1,14 @@
-# from rest_framework import permissions
+from rest_framework import permissions
 
 
-# class IsOwnerOrReadOnly(permissions.BasePermission):
-#     """
-#     Custom permission class to allow only question owners update them.
-#     """
+class IsOwnerOrReadOnly(permissions.BasePermission):
+    """
+    Custom permission class to allow only question owners update them.
+    """
 
-#     def has_object_permission(self, request, view, obj):
-#         # Read permissions are allowed to any request
-#         if request.method in permissions.SAFE_METHODS:
-#             return True
-#         # Write permissions are only allowed to the owner
-#         return obj.created_by == request.user
+    def has_object_permission(self, request, view, obj):
+        # Read permissions are allowed to any request
+        if request.method in permissions.SAFE_METHODS:
+            return True
+        # Write permissions are only allowed to the owner
+        return obj.created_by == request.user
