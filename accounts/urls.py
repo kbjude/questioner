0c388e9566e555994c9014757f96bc07899a7ed8
Innocent Lou <bisonlou@gmail.com>
2019-03-20 15:01:32 +0300

    Modified Readme

--- accounts/urls.py
+++ accounts/urls.py
@@ -3,8 +3,8 @@ from . import views
 
 urlpatterns = [
 
-    path('login', views.Login.as_view(), name='login'),
-    path('signup', views.SignUp.as_view(), name='signup'),
+    path('login/', views.Login.as_view(), name='login'),
+    path('signup/', views.SignUp.as_view(), name='signup'),
     path("session/signup/", views.SignUp.as_view(), name="signup2"),
     path("session/profile/", views.profile.as_view(), name="profile"),
 
