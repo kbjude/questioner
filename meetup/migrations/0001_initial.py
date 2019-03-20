db233eb2d91ecf5c8f2cef18bfeb259b003045cc
Innocent Lou <bisonlou@gmail.com>
2019-03-20 18:12:00 +0300

    Made meetup body non nullable

--- meetup/migrations/0001_initial.py
+++ meetup/migrations/0001_initial.py
@@ -19,7 +19,7 @@ class Migration(migrations.Migration):
             fields=[
                 ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
                 ('title', models.CharField(max_length=50)),
-                ('body', models.TextField(null=True)),
+                ('body', models.TextField()),
                 ('date', models.DateField()),
                 ('start', models.TimeField()),
                 ('end', models.TimeField()),
