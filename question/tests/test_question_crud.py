eb5e031f48787efaeaa9edb104276d4a1b008be3
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-03-21 14:30:54 +0300

    Update test_question_crud.py

--- question/tests/test_question_crud.py
+++ question/tests/test_question_crud.py
@@ -3,8 +3,6 @@ import json
 from django.contrib.auth.models import User
 from django.test import TestCase
 from rest_framework.test import APIClient
-from django.urls import reverse
-
 from meetup.models import Meeting
 from question.models import Question
 
