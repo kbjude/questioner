888cb5b46a8e139fcf7163e963a967c669c55c76
Kalule Arthur <kalulearthur@gmail.com>
2019-03-21 13:36:17 +0300

    Add Vote and Comment Models to Django admin

--- question/admin.py
+++ question/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
 
-from .models import Question
+from .models import Question, Comment,Vote
 
 admin.site.register(Question)
+admin.site.register(Comment)
+admin.site.register(Vote)
+
+
 
 # Register your models here.
