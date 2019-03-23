92343911ed546f13e4c8fcb4a383b2b10a57ae9b
Mugaya Joel Patrick <43677190+PatrickMugayaJoel@users.noreply.github.com>
2019-03-23 19:40:53 +0300

    Update models.py

--- tag/models.py
+++ tag/models.py
@@ -8,9 +8,12 @@ class Tag(models.Model):
     title = models.CharField(max_length=50, unique=True)
     created_by = models.ForeignKey(User, on_delete=models.PROTECT)
     active = models.BooleanField(default=True)
-
-    def __str__(self):
-        return (self.title)
+    meetups = models.ManyToManyField(
+        Meeting,
+        through='MeetingTag',
+        related_name='rest_tags',
+        through_fields=('tag', 'meetup'),
+    )
 
 
 class MeetingTag(models.Model):
